2874d0518f4c0a2090494963d82135c02174e159
Tevvek <daniil@poyda-systems.com>
2024-07-02 15:39:55 +0200

    Rename podcasts list to carousel

--- src/components/PodcastsCarousel.vue
+++ src/components/PodcastsCarousel.vue
@@ -4,7 +4,7 @@
         '--swiper-pagination-bullet-inactive-color': '#fff',
         '--swiper-pagination-bullet-inactive-opacity': 1
     }" wrapper-class="pb-10 pt-2">
-        <swiper-slide v-for="(podcast, key) in filteredPodcasts" :key="key" class="px-4">
+        <swiper-slide v-for="(podcast, key) in filteredPodcasts" :key="key" class="px-8">
             <div class="bg-white rounded-xl p-4 h-44 flex gap-x-4 transition duration-300 hover:scale-105">
                 <img :src="podcast.image" alt="podcast" class="rounded-lg">
                 <div class="flex flex-col gap-y-1">
