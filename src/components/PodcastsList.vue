8b4e60a8e1d88b50b86b7cccbb33796e5f1d3f45
Tevvek <daniil@poyda-systems.com>
2024-07-02 14:28:09 +0200

    Create mock projects. Make workabale projects swiper.

--- src/components/PodcastsList.vue
+++ src/components/PodcastsList.vue
@@ -1,3 +1,36 @@
-<template></template>
+<template>
+    <swiper :slides-per-view="3" :pagination="{ clickable: true }" :modules="modules" :loop="true" :style="{
+        '--swiper-pagination-color': '#155e75',
+        '--swiper-pagination-bullet-inactive-color': '#fff',
+        '--swiper-pagination-bullet-inactive-opacity': 1
+    }" wrapper-class="pb-10 pt-2" :centered-slides="true">
+        <swiper-slide v-for="(podcast, key) in podcasts" :key="key" class="px-4">
+            <div class="bg-white rounded-xl p-4 h-44 flex gap-x-4 transition duration-300 hover:scale-105">
+                <img :src="podcast.image" alt="podcast" class="rounded-lg">
+                <div class="flex flex-col gap-y-1">
+                    <h3 class="font-bold">{{ podcast.title }}</h3>
+                    <p class="text-sm line-clamp-6" :title="podcast.description">{{ podcast.description }}</p>
+                </div>
+            </div>
+        </swiper-slide>
+    </swiper>
+</template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Pagination } from 'swiper/modules';
+
+const modules = [Pagination];
+interface Podcast {
+    title: string;
+    description: string;
+    image: string;
+}
+
+const props = defineProps<{
+    podcasts: Podcast[]
+}>();
+
+
+const { podcasts } = props;
+</script>
