a4a6cacb4d43622718d6b0fba447dd1635b70347
Tevvek <daniil@poyda-systems.com>
2024-09-20 01:54:15 +0200

    Adapt to use content collections instead of pages. Bring one podcast into a different folder to check. Let's proceed to move all podcasts.

---
+++ src/content/config.ts
@@ -0,0 +1,18 @@
+// Import utilities from `astro:content`
+import { z, defineCollection } from "astro:content";
+// Define a `type` and `schema` for each collection
+const podcastsCollection = defineCollection({
+  type: "content",
+  schema: z.object({
+    title: z.string(),
+    shortDescription: z.string(),
+    longDescription: z.string(),
+    theme: z.string(),
+    image: z.string(),
+    link: z.string(),
+  }),
+});
+// Export a single `collections` object to register your collection(s)
+export const collections = {
+  podcasts: podcastsCollection,
+};
