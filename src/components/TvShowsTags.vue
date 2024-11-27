1acab20c290cef3cdffe4f04d30e249e70a543cf
Tevvek <daniil@poyda-systems.com>
2024-11-27 20:20:04 +0100

    Add description under podcasts and tvshows in resources page. Fix some styles along the way.

--- src/components/TvShowsTags.vue
+++ src/components/TvShowsTags.vue
@@ -1,6 +1,6 @@
 <template>
     <div
-        class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 auto-rows-fr xl:flex gap-4 py-5 justify-end text-cyan-800 px-4 md:px-8">
+        class="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 auto-rows-fr xl:flex gap-4 mt-5 py-5 text-cyan-800 px-4 md:px-8">
         <button :class="[
             'px-4 py-1 rounded-2xl transition duration-300 text-sm font-semibold shadow-md',
             buttonClass(tag)
