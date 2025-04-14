bbdef0efa80d0d700d50ac5482b7263a4a150208
Tevvek <daniil@poyda-systems.com>
2025-04-14 17:09:28 +0200

    * Added a register utils function to the utils auth file * Created register e2e tests

--- tests/utils/auth.ts
+++ tests/utils/auth.ts
@@ -1,4 +1,4 @@
-import type { Page, BrowserContext } from "@playwright/test";
+import type { BrowserContext, Page } from "@playwright/test";
 
 // Log in with given credentials and wait for redirect
 export const login = async (
@@ -21,3 +21,16 @@ export const getTokenCookie = async (context: BrowserContext) => {
   const cookies = await context.cookies();
   return cookies.find((c) => c.name === "english-for-abroad-token");
 };
+
+export async function register(page: Page, email: string, password: string) {
+  await page.goto("/register");
+
+  await page.fill('input[name="email"]', email);
+  await page.fill('input[name="password"]', password);
+  await page.fill('input[name="repeat-password"]', password);
+
+  await Promise.all([
+    page.waitForURL("/dashboard"),
+    page.click('button[type="submit"]'),
+  ]);
+}
