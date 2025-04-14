d38332cabee5fc9009dcc0b9ce35885a9565d0f6
Tevvek <daniil@poyda-systems.com>
2025-04-14 18:30:57 +0200

    * Move DashboardTopBar from vue to astro * Crated ProfileDropdown and BurgerMenu components in vue because they are the minimum part that requires interactivity

---
+++ src/components/dashboard/BurgerMenu.vue
@@ -0,0 +1,11 @@
+<template>
+    <button type="button" class="-m-2.5 p-2.5 text-gray-700 lg:hidden" @click="sidebarOpen.set(true)">
+        <span class="sr-only">Open sidebar</span>
+        <Bars3Icon class="size-6" aria-hidden="true" />
+    </button>
+</template>
+
+<script setup lang="ts">
+import { Bars3Icon } from "@heroicons/vue/24/outline";
+import { sidebarOpen } from "./dashboard-store";
+</script>
