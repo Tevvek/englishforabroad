ae82de3d9649ae8e56395e44c6b031566eb24589
Tevvek <daniil@poyda-systems.com>
2025-04-24 12:25:11 +0200

    WIP

--- src/components/calendar/SelectedClasses.vue
+++ src/components/calendar/SelectedClasses.vue
@@ -19,33 +19,40 @@ const store = useCalendarStore();
 const { hasLockedDates, hasSelectedDates, selectedDates, groupWeekly, allLocked } = storeToRefs(store);
 const { toggleSelectedDate, toggleLock, toggleLockAll, resetDates } = store;
 
-watch(
-    selectedDates,
-    () => {
-        if (!groupWeekly) return;
+// watch(
+//     selectedDates,
+//     () => {
+//         if (!groupWeekly) return;
 
-        const leavingEls = selectedDatesRef.value?.children;
-        if (leavingEls) {
-            Array.from(leavingEls).forEach((el) => {
-                const element = el as HTMLElement;
-                const top = element.offsetTop;
-                element.style.top = `${top}px`;
-                element.style.gridTemplateColumns = "auto 1fr auto"; // this is the same as the grid-cols-subgrid class
-            });
-        }
-    },
-    { deep: true }
-);
+//         const leavingEls = selectedDatesRef.value?.children;
+//         if (leavingEls) {
+//             Array.from(leavingEls).forEach((el) => {
+//                 const element = el as HTMLElement;
+//                 const top = element.offsetTop;
+//                 element.style.top = `${top}px`;
+//                 element.style.gridTemplateColumns = "auto 1fr 1fr max-content"; // this is the same as the grid-cols-subgrid class
+//             });
+//         }
+//     },
+//     { deep: true }
+// );
+
+function onLeave(el: Element) {
+    const element = el as HTMLElement;
+    const top = element.offsetTop;
+    element.style.top = `${top}px`;
+}
+
 </script>
 
 <template>
     <!-- SELECTED CLASSES -->
     <div class="mt-6 space-y-2 lg:col-start-2">
         <h3 class="text-sm font-semibold text-gray-700">Your selected classes</h3>
-        <ul class="relative grid grid-cols-[auto_1fr_auto]" ref="selectedDatesRef">
-            <TransitionGroup name="fade-slide">
+        <ul class="relative grid grid-cols-[auto_1fr_1fr_max-content]" ref="selectedDatesRef">
+            <TransitionGroup name="fade-slide" @before-leave="onLeave">
                 <li v-if="hasSelectedDates"
-                    class="w-full text-sm text-gray-600 py-1 gap-2 border-gray-300 border-b mb-1 pb-2 grid grid-cols-subgrid col-span-3">
+                    class="w-full text-sm text-gray-600 py-1 gap-2 border-gray-300 border-b mb-1 pb-2 grid grid-cols-subgrid col-span-4 items-center">
                     <button @click="toggleLockAll" :class="cn(
                         'text-gray-400 hover:text-gray-600 transition-transform duration-300',
                     )">
@@ -54,6 +61,9 @@ watch(
                     </button>
 
                     <span>Dates</span>
+
+                    <!-- TIME -->
+                    <input type="time" class="border rounded px-2 py-1 text-xs text-gray-700 disabled:opacity-50" />
 
                     <button @click="resetDates" :class="cn(
                         'text-xs text-red-500 hover:underline transition justify-self-end',
@@ -65,7 +75,7 @@ watch(
 
                 <!-- DATES -->
                 <li v-for="item in selectedDates" :key="item.date"
-                    class="w-full text-sm text-gray-600 py-1 gap-2 grid col-span-3 grid-cols-subgrid ">
+                    class="w-full text-sm text-gray-600 py-1 gap-2 grid col-span-4 grid-cols-subgrid items-center">
 
                     <!-- LOCK -->
                     <button @click="toggleLock(item.date)" :class="cn(
@@ -79,6 +89,11 @@ watch(
                     <!-- DATE -->
                     <span>{{ formatDate(item.date) }}</span>
 
+                    <!-- TIME -->
+                    <input type="time" class="border rounded px-2 py-1 text-xs text-gray-700 disabled:opacity-50"
+                        :disabled="item.locked" />
+
+
                     <!-- REMOVE -->
                     <button @click="toggleSelectedDate(item.date)" :class="cn(
                         'text-xs text-red-500 hover:underline transition justify-self-end',
@@ -88,7 +103,7 @@ watch(
                     </button>
                 </li>
                 <!-- MESSAGE -->
-                <li v-if="!hasSelectedDates" class="text-sm text-gray-500 py-1 grid col-span-3">
+                <li v-if="!hasSelectedDates" class="text-sm text-gray-500 py-1 grid col-span-4">
                     No classes selected
                 </li>
             </TransitionGroup>
@@ -104,7 +119,7 @@ watch(
 .fade-slide-move,
 .fade-slide-enter-active,
 .fade-slide-leave-active {
-    transition: all 0.3s ease;
+    transition: all 500s ease;
 }
 
 .fade-slide-enter-from,
