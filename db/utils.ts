01bc9958da74875a43300d2c18562edf6b03b8dc
Tevvek <daniil@poyda-systems.com>
2025-10-24 18:19:52 +0200

    Add warning comment in db/utils.ts regarding immutability with defineTable
    - Included a cautionary note about the use of immutable data libraries with defineTable to prevent errors related to object extensibility.

--- db/utils.ts
+++ db/utils.ts
@@ -1,5 +1,9 @@
 import { column, defineTable } from "astro:db";
 
+// Warning: Do not use immer or other immutable data libraries with defineTable
+// as the returned table object needs to be mutable. Using immutable data will
+// result in "Cannot add property getName, object is not extensible" errors.
+
 /**
  * Extended defineTable that automatically adds createdAt and updatedAt fields
  * @param config - Table configuration object
