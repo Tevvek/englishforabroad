94b4156206bd92bdf1ba8835b325ff6d5353e4a5
Tevvek <daniil@poyda-systems.com>
2025-10-25 21:55:51 +0200

    Add login action to server exports
    - Integrated the `login` action from the login module into the server exports in the actions index file, enhancing the overall functionality for user authentication.

--- src/actions/index.ts
+++ src/actions/index.ts
@@ -1,7 +1,9 @@
+import { login } from "@/pages/login/_actions/login-action";
 import { freebie } from "./freebie";
 import { waitlist } from "./waitlist";
 
 export const server = {
   freebie,
   waitlist,
+  login,
 };
