3c70fb91f6ce06bd7926e352d96481af3990b615
Daniil Teterevkov <daniil.teterevkov@ingrammicro.com>
2025-12-31 17:18:26 +0100

    Create stripe client (althoguh used in server)

---
+++ src/lib/stripe.ts
@@ -0,0 +1,7 @@
+import Stripe from "stripe";
+import { STRIPE_SECRET_KEY } from "astro:env/server";
+
+export const stripe = new Stripe(STRIPE_SECRET_KEY, {
+  apiVersion: "2025-12-15.clover",
+  typescript: true,
+});
