8d231cc0653da176a5a1c57651c32975d3251b11
Tevvek <daniil@poyda-systems.com>
2026-02-17 12:31:29 +0100

    Remove user check on pages in dashboard as middleware will be check and set in locals if signed in

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,7 +1,6 @@
 /// <reference path="../.astro/types.d.ts" />
 
 declare namespace App {
-  // Note: 'import {} from ""' syntax does not work in .d.ts files.
   type AuthUser = import("better-auth").User & {
     stripeCustomerId?: string | null;
   };
